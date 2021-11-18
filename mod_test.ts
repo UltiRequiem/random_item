5362bcc69d575ee0484bef57b0b17275184e2432
GitHub <noreply@github.com>
2021-11-18 01:06:41 +0000

    :arrow_up: Update Dependencies

--- mod_test.ts
+++ mod_test.ts
@@ -2,7 +2,7 @@ import {
   assert,
   assertEquals,
   assertThrows,
-} from "https://deno.land/std@0.114.0/testing/asserts.ts";
+} from "https://deno.land/std@0.115.1/testing/asserts.ts";
 import randomItem, { randomMultipleItems } from "./mod.ts";
 
 const letters = ["a", "b", "c", "d", "e"];
