aa8ea3b27dc57cdef785357c969272121203bce5
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-03-21 16:45:58 -0500

    :sparkles: Node Support

---
+++ script/node_support.ts
@@ -0,0 +1,55 @@
+import { build } from "https://deno.land/x/dnt@0.22.0/mod.ts";
+
+const publisher = {
+  name: "Eliaz Bobadilla",
+  username: "ultirequiem",
+  email: "eliaz.bobadilladev@gmail.com",
+  site: "https://ultirequiem.com",
+} as const;
+
+const packageConfig = {
+  name: "@ultirequiem/random-item",
+  repoName: "random_item",
+  version: "1.0.1",
+  description: "Get a random item from an array",
+  keywords: ["random", "array"],
+  license: "MIT",
+  homepage: "https://random-item.js.org",
+} as const;
+
+await build({
+  entryPoints: ["./mod.ts"],
+
+  outDir: "./node",
+
+  shims: {
+    deno: true,
+  },
+
+  package: {
+    name: packageConfig.name,
+    description: packageConfig.description,
+
+    version: packageConfig.version,
+
+    license: packageConfig.license,
+
+    funding: {
+      type: "patreon",
+      url: `https://www.patreon.com/${publisher.username}`,
+    },
+
+    repository: `github:${publisher.username}/${packageConfig.repoName}`,
+    homepage: packageConfig.homepage,
+
+    bugs: {
+      url: `https://github.com/${publisher.username}/${packageConfig.repoName}/issues`,
+      email: publisher.email,
+    },
+
+    keywords: packageConfig.keywords,
+  },
+});
+
+Deno.copyFileSync("license", "node/LICENSE");
+Deno.copyFileSync("readme.md", "node/README.md");
