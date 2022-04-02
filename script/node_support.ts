d93525528fa75d5b20e1cc7e8480e0b5c9b9c0fa
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-04-02 08:29:20 -0500

    :bug: Fix bug on script

--- script/node_support.ts
+++ script/node_support.ts
@@ -1,57 +1,11 @@
 import { buildPackage } from "https://deno.land/x/ultirequiem@0.0.12/node_support.ts";
 
-const packageConfig = {
-  name: "@ultirequiem/random-item",
-  repoName: "random_item",
-  version: "1.0.1",
-  description: "Get a random item from an array",
-  keywords: ["random", "array"],
-  license: "MIT",
-  homepage: "https://random-item.js.org",
-} as const;
-
 buildPackage({
   repoName: "random_item",
   name: "@ultirequiem/random-item",
   description: "Get a random item from an array.",
   homepage: "https://random-item.js.org",
   keywords: ["random", "array"],
+  license: "MIT",
+  version: "1.1.1",
 });
-
-await build({
-  entryPoints: ["./mod.ts"],
-
-  outDir: "./node",
-
-  shims: {
-    deno: true,
-  },
-
-  package: {
-    name: packageConfig.name,
-    description: packageConfig.description,
-
-    version: packageConfig.version,
-
-    license: packageConfig.license,
-
-    funding: {
-      type: "patreon",
-      url: `https://www.patreon.com/${publisher.username}`,
-    },
-
-    repository: `github:${publisher.username}/${packageConfig.repoName}`,
-    homepage: packageConfig.homepage,
-
-    bugs: {
-      url:
-        `https://github.com/${publisher.username}/${packageConfig.repoName}/issues`,
-      email: publisher.email,
-    },
-
-    keywords: packageConfig.keywords,
-  },
-});
-
-Deno.copyFileSync("license", "node/LICENSE");
-Deno.copyFileSync("readme.md", "node/README.md");
