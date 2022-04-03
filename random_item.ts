52fb0ff49621c2adf7dd8b7c2181585084d42bfc
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-04-03 16:06:02 -0500

    :bookmark: v1.2.0

---
+++ random_item.ts
@@ -0,0 +1,14 @@
+import { Command } from "https://deno.land/x/cliffy@v0.22.2/command/mod.ts";
+import { randomItem } from "./mod.ts";
+
+const root = new Command()
+  .name("random_item")
+  .version("0.1.0")
+  .description("Get a random item from a list")
+  .arguments("<items....:string>");
+
+const { args } = await root.parse(Deno.args);
+
+const selectedItem = randomItem(args[0]);
+
+console.log(selectedItem);
--- script/node_support.ts
+++ script/node_support.ts
@@ -1,11 +1,21 @@
 import { buildPackage } from "https://deno.land/x/ultirequiem@0.0.12/node_support.ts";
 
-buildPackage({
-  repoName: "random_item",
-  name: "@ultirequiem/random-item",
-  description: "Get a random item from an array.",
-  homepage: "https://random-item.js.org",
-  keywords: ["random", "array"],
-  license: "MIT",
-  version: "1.1.1",
-});
+buildPackage(
+  {
+    repoName: "random_item",
+    name: "@ultirequiem/random-item",
+    description: "Get a random item from an array.",
+    homepage: "https://random-item.js.org",
+    keywords: ["random", "array"],
+    license: "MIT",
+    version: "1.2.0",
+  },
+  {
+    check: false,
+    supportCJS: false,
+    entryPoints: [
+      "./mod.ts",
+      { name: "random_item", path: "./random_item.ts", kind: "bin" },
+    ],
+  }
+);
